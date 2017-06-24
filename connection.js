d5861080eecd578496eb96d9ec7ddf58f25532ea
Dmitriy <dmitriy.shmilo@gmail.com>
2017-06-24 23:41:13 +0300

    #3 - Added Connection stub.

---
+++ connection.js
@@ -0,0 +1,25 @@
+module.exports = class Connection {
+    constructor(connectionString) {
+        throw new Error("Not implemented");
+    }
+
+    get() {
+        throw new Error("Not implemented");
+    }
+
+    all() {
+        throw new Error("Not implemented");
+    }
+
+    run() {
+        throw new Error("Not implemented");
+    }
+
+    exec() {
+        throw new Error("Not implemented");
+    }
+
+    close() {
+        throw new Error("Not implemented");
+    }
+};
