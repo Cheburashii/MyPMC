710da03f346cb7bc1a647320f2ceea7dfe91132f
Dmitriy <dmitriy.shmilo@gmail.com>
2017-06-25 01:26:37 +0300

    #7 - Added DataMapper stub.

---
+++ data-mapper.js
@@ -0,0 +1,13 @@
+module.exports = class DataMapper {
+    constructor(definitions) {
+        throw new Error("Not implemented");
+    }
+
+    mapFromRow(definitionName, row) {
+        throw new Error("Not implemented");
+    }
+
+    mapToArgs(definitionName, object, cols) {
+        throw new Error("Not implemented");
+    }
+};
