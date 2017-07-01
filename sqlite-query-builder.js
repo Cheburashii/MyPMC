88283dda02d3360190d982862ac5246e78cd5808
Cheburashii <evgeniy.konstantinov.02@mail.ru>
2017-07-01 20:31:58 +0300

    #5 Implemented sqlite query builder.

--- sqlite-query-builder.js
+++ sqlite-query-builder.js
@@ -1,18 +1,49 @@
 module.exports = class SQLiteQueryBuilder {
 
     static buildInsert(table, cols) {
-        throw new Error("Not implemented");
+        if (!table || !table.trim()) {
+            throw new Error("Table name can`t be empty.");
+        } else if (!cols || cols.length === 0) {
+            throw new Error("Cols names can`t be empty.");
+        }
+        return `INSERT INTO ${table}(${cols.join(", ")}) VALUES(${"?, ".repeat(cols.length).slice(0, -2)})`;
     }
 
     static buildUpdate(table, cols, condition) {
-        throw new Error("Not implemented");
+        if (!table || !table.trim()) {
+            throw new Error("Table name can`t be empty.");
+        } else if (!cols || cols.length === 0) {
+            throw new Error("Cols names can`t be empty.");
+        }
+        let query = `UPDATE ${table} SET ${cols.map(item => `${item} = ?`).join(", ")}`;
+        if (condition) {
+            query += ` WHERE ${condition}`;
+        }
+        return query;
     }
 
     static buildDelete(table, condition) {
-        throw new Error("Not implemented");
+        if (!table || !table.trim()) {
+            throw new Error("Table name can`t be empty.");
+        }
+        return `DELETE FROM ${table} ${condition
+            ? `WHERE ${condition}`
+            : ""}`;
     }
 
     static buildSelect(table, cols, condition, order) {
-        throw new Error("Not implemented");
+        if (!table || !table.trim()) {
+            throw new Error("Table name can`t be empty.");
+        }
+        let query = `SELECT ${!cols || cols.length === 0
+            ? "*"
+            : cols.join(", ")} FROM ${table} `;
+        if (condition) {
+            query += `WHERE ${condition} `;
+        }
+        if (order) {
+            query += `ORDER BY ${order}`;
+        }
+        return query;
     }
 };
