8e12ba9838bb39933b1e5f1a6bc96d9fb90856e0
Cheburashii <evgeniy.konstantinov.02@mail.ru>
2017-07-06 09:32:17 +0300

    #11 Wrote detailsGET and boilerplates for other controllers

--- controllers/properties-controller.js
+++ controllers/properties-controller.js
@@ -2,6 +2,7 @@ const Connection = require("../model/connection");
 const PropertiesDAO = require("../model/properties-dao");
 const join = require("path").join;
 const DATABASE_PATH = join(__dirname, "../database.sqlite3");
+
 module.exports = class PropertiesController {
     constructor() {
         this.connection = new Connection(DATABASE_PATH);
@@ -17,5 +18,28 @@ module.exports = class PropertiesController {
                 res.sendStatus(500);
                 console.log(err);
             });
+    }
+
+    addPUT(req, res, next) {
+        throw new Error("Not implemented");
+    }
+
+    detailsGET(req, res, next) {
+        this.dao.getById(req.params.id)
+            .then((row) => {
+                res.render("details", row);
+            })
+            .catch((err) => {
+                res.sendStatus(500);
+                console.log(err);
+            });
+    }
+
+    updatePOST(req, res, next) {
+        throw new Error("Not implemented");
+    }
+
+    removeDELETE(req, res, next) {
+        throw new Error("Not implemented");
     }
 };
