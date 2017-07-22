aab52057804133352cb73ef23e729b1cd7379c43
Cheburashii <evgeniy.konstantinov.02@mail.ru>
2017-07-22 20:33:03 +0300

    #14 Changed file with routing

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const propertiesController = new PropertiesController();
 const bookingsController = new BookingsController();
 
-app.engine("hbs", require("express-handlebars")({
+app.engine("hbs", expressHandlebars({
     extname: ".hbs"
 }));
 app.set("view engine", "hbs");
@@ -24,7 +24,7 @@ app.get("/properties/:id", (req, res, next) => {
     propertiesController.detailsGET(req, res, next);
 });
 
-app.put("/properties/:id/add", (req, res, next) => {
+app.post("/properties/add", (req, res, next) => {
     propertiesController.addPUT(req, res, next);
 });
 
@@ -36,15 +36,15 @@ app.delete("/properties/:id/remove", (req, res, next) => {
     propertiesController.removeDELETE(req, res, next);
 });
 
-app.get("/bookings/:id", (req, res, next) => {
+app.get("/properties/:id/bookings", (req, res, next) => {
     bookingsController.getByPropertyIdGET(req, res, next);
 });
 
-app.put("/bookings/:id/add", (req, res, next) => {
+app.put("/bookings/add", (req, res, next) => {
     bookingsController.addPUT(req, res, next);
 });
 
-app.delete("/bookings:id/remove", (req, res, next) => {
+app.delete("/bookings/:id/remove", (req, res, next) => {
     bookingsController.removeDELETE(req, res, next);
 });
 app.listen(1339);
